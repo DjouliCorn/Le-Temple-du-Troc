dca2d2a8ad0c56c243aa1706e84cd56914243d4e
DjouliCorn <tissier.julie@gmail.com>
2021-01-29 13:46:57 +0100

    réparation des bugs login

--- client/login.php
+++ client/login.php
@@ -9,24 +9,31 @@ if (isset($_POST['connexion'])) {
     if (empty($_POST['pseudo'])) {
         $errors['pseudo'] = 'Veuillez entrer votre pseudo';
     }
-    if (empty($_POST['password'])) {
-        $errors['password'] = 'Veuillez entrer le mot de passe';
+
+    if (empty($_POST['motDePasse'])) {
+        $errors['motDePasse'] = 'Veuillez entrer le mot de passe';
+        
     }
     $username = $_POST['pseudo'];
     $password = $_POST['motDePasse'];
+
+    echo $username;
+    echo $password;
+
+    echo "error : ".count($errors);
 
 
 
 
     if (count($errors) === 0) {
-        $query = "SELECT userName, motDePasse FROM Clients WHERE userName = '".$username."'";
+        $query = "SELECT userName, motDePasse FROM clients WHERE userName = '".$username."'";
 
         $resultat = mysqli_query($dbh,$query);
 
         foreach ($resultat as $elt){
 
             if($elt['userName'] == $username && password_verify($password, $elt['motDePasse'])){
-                require_once './indexClient.php';
+                require_once 'indexClient.php';
                 echo 'Vous êtes connecté.' ;
 
             } else {
