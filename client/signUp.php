7806498d01a27e9d8052c10f184b8c44418ad9f7
DjouliCorn <tissier.julie@gmail.com>
2021-02-02 12:28:31 +0100

    sign up gestion erreur de formulaire

--- client/signUp.php
+++ client/signUp.php
@@ -1,6 +1,6 @@
 <?php
 
-  session_start();
+session_start();
 
 include '../inc/accessBDD.php';
 
@@ -18,18 +18,32 @@ if (isset($_POST['signUpBtn'])) {
 
 
     if (empty($_POST['pseudo'])) {
+
         $errors['pseudo'] = 'Pseudo requis';
     }
     if (empty($_POST['email'])) {
+
         $errors['email'] = 'Email requis';
     }
     if (empty($_POST['motDePasse'])) {
+
         $errors['motDePasse'] = 'Mot de passe requis';
     }
-    if (isset($_POST['motDePasse']) && $_POST['motDePasse'] !== $_POST['motDePasseConf']) {
-        $errors['motDePasseConf'] = 'Les deux mots de passe ne correspondent pas';
+
+    if (empty($_POST['dateNaiss'])) {
+
+        $errors['dateNaiss'] = 'Date de naissance requise';
     }
 
+    if (empty($_POST['ville'])) {
+
+        $errors['ville'] = 'Ville requise';
+    }
+
+    if (isset($_POST['motDePasse']) && $_POST['motDePasse'] !== $_POST['motDePasseConf']) {
+
+        $errors['motDePasseConf'] = 'Les deux mots de passe ne correspondent pas';
+    }
     $username = $_POST['pseudo'];
     $email = $_POST['email'];
     $nom =  $_POST['nomClient'];
@@ -38,26 +52,34 @@ if (isset($_POST['signUpBtn'])) {
     $ville =  $_POST['ville'];
     $password = password_hash($_POST['motDePasse'], PASSWORD_DEFAULT); //encrypt password
 
-    $queryCheckUserName = "SELECT userName FROM Clients WHERE $username LIKE userName";
-    $resultatUserName = mysqli_query($dbh, $queryCheckUserName);
     
-    if (!$resultatUserName) {
-        include './form_signUp.php';
+
+    $queryCheckUserName = "SELECT userName FROM Clients WHERE userName = '" . $username . "'";
+    $resultatUserName = mysqli_query($dbh, $queryCheckUserName);
+
+    if (mysqli_num_rows($resultatUserName) != 0) {
         $errors['pseudo'] = 'Ce nom d`utilisateur existe déjà, veuillez en choisir un autre';
-        echo '<p style="text-align: center">' . $errors['pseudo'] . '</p>';
+    }
+    
+    if (count($errors) > 0) {
+        include './form_signUp.php';
+
+        foreach ($errors as $error) {
+
+            echo '<p style="text-align: center">' . $error . '</p>';
+        }
     }
 
-    else if (count($errors) === 0) {
-        
-        $query = "INSERT INTO Clients SET userName=?, email=?, nomClient=?, prenomClient=?, dateNaiss=?, ville=?, motDePasse=?";
-        $stmt = $dbh->prepare($query);
-        $stmt->bind_param('sssssss', $username, $email, $nom, $prenom, $dateNaiss, $ville, $password);
-        $result = $stmt->execute();
+        if (count($errors) === 0) {
 
-        include './form_login.php';
+            $query = "INSERT INTO Clients SET userName=?, email=?, nomClient=?, prenomClient=?, dateNaiss=?, ville=?, motDePasse=?";
+            $stmt = $dbh->prepare($query);
+            $stmt->bind_param('sssssss', $username, $email, $nom, $prenom, $dateNaiss, $ville, $password);
+            $result = $stmt->execute();
+
+            include './form_login.php';
+        }
+
+        $dbh = null;
     }
-
-    $dbh = null;
-}
-
 
