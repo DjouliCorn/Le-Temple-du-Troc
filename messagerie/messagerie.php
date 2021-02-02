4362230db1371a122bdca7871f8ed9b3a44445ec
jesusa <justinjhes@gmail.com>
2021-02-02 09:26:35 +0100

    bug fixed message envoyé

--- messagerie/messagerie.php
+++ messagerie/messagerie.php
@@ -42,9 +42,7 @@ $messagesRecu="";
  foreach($messages as $message){
 //echo $message['createdAt'];
    
-        $messagesEnvoye .="<tr><td>".$message['nomProduit']."</td><td>".$message['email']."</td><td>".$message['content']."</td><td>".$message['createdAt']."</td>".'<td><button class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-reply" viewBox="0 0 16 16">
-        <path d="M6.598 5.013a.144.144 0 0 1 .202.134V6.3a.5.5 0 0 0 .5.5c.667 0 2.013.005 3.3.822.984.624 1.99 1.76 2.595 3.876-1.02-.983-2.185-1.516-3.205-1.799a8.74 8.74 0 0 0-1.921-.306 7.404 7.404 0 0 0-.798.008h-.013l-.005.001h-.001L7.3 9.9l-.05-.498a.5.5 0 0 0-.45.498v1.153c0 .108-.11.176-.202.134L2.614 8.254a.503.503 0 0 0-.042-.028.147.147 0 0 1 0-.252.499.499 0 0 0 .042-.028l3.984-2.933zM7.8 10.386c.068 0 .143.003.223.006.434.02 1.034.086 1.7.271 1.326.368 2.896 1.202 3.94 3.08a.5.5 0 0 0 .933-.305c-.464-3.71-1.886-5.662-3.46-6.66-1.245-.79-2.527-.942-3.336-.971v-.66a1.144 1.144 0 0 0-1.767-.96l-3.994 2.94a1.147 1.147 0 0 0 0 1.946l3.994 2.94a1.144 1.144 0 0 0 1.767-.96v-.667z"/>
-      </svg></button> </td> <td><button class="btn btn-secondary"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+        $messagesEnvoye .="<tr><td>".$message['nomProduit']."</td><td>".$message['email']."</td><td>".$message['content']."</td><td>".$message['createdAt']."</td>".' </td> <td><button class="btn btn-secondary"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
         <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
       </svg> </button></td> </tr>';
@@ -102,7 +100,7 @@ $messagesRecu="";
   <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">envoye
 	<table class="table">
 		<thead>
-		<tr> <th scope="col">Nom produit</th>  <th scope="col">Email</th>  <th scope="col">Message</th>  <th scope="col">Date</th> <th>Repondre</th> <th>supprimer</th> </tr>
+		<tr> <th scope="col">Nom produit</th>  <th scope="col">Email</th>  <th scope="col">Message</th>  <th scope="col">Date</th>  <th>supprimer</th> </tr>
 		</thead>
 		<tbody>
 
